d9df58100d8d8baf0be3d72a5b637fa2638617d4
Pranav shende <pranavshende@users.noreply.github.com>
2026-08-21 21:01:31 +0530

    Fix Prisma client initialization

--- backend/prismaClient.js
+++ backend/prismaClient.js
@@ -1,8 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 require('dotenv').config();
 
-const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL
-});
+const prisma = new PrismaClient();
 
 module.exports = prisma;
